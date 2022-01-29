5d67b577773d40bcd9f52e2ec6048bcc3d7bc323
GalaxyVN <phamgiahuy092@gmail.com>
2022-01-29 18:23:09 +0700

    b

--- src/main/java/tech/ateliermc/atelier/Atelier.java
+++ src/main/java/tech/ateliermc/atelier/Atelier.java
@@ -1,8 +1,15 @@
 package tech.ateliermc.atelier;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Atelier implements DedicatedServerModInitializer {
+    
+    public MinecraftServer server;
+    public Logger LOGGER = LogManager.getLogger("Atelier");
+
     @Override
     public void onInitializeServer() {
 
